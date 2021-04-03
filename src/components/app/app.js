e57bc1032ff752385025e291169bdfed4427ba0c
Vika <queenviktoriyphp@gmail.com>
2021-04-03 19:28:15 +0300

    componentDidCatch in people-page, and use function to add other lists

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,78 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator/error-indicator';
 import Header from '../header';
 import ItemList from '../item-list';
+import PeoplePage from '../people-page';
 import PersonDetails from '../person-details';
 import RandomPlanet from '../random-planet';
 import './app.css';
 
 export default class App extends Component {
+  swapiService = new SwapiService();
   state = {
-    selectedPerson: null,
+    showRandomPlanet: true,
+
+    hasError: false,
   };
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id,
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet,
+      };
     });
   };
+
+  componentDidCatch() {
+    console.log('componentDidCatch()');
+    this.setState({ hasError: true });
+  }
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
     return (
-      <div>
+      <div className="stardb-app">
         <Header />
-        <RandomPlanet />
+        {planet}
+        {/* <RandomPlanet /> */}
+
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}
+          >
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
+        </div>
         {/* <ButtonToggle /> */}
+        <PeoplePage />
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
+            <ErrorButton />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+            <ErrorButton />
           </div>
         </div>
       </div>
---
+++ src/components/people-page/people-page.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import './people-page.css';
+export default class PeoplePage extends Component {
+  swapiService = new SwapiService();
+  state = {
+    selectedPerson: 3,
+    hasError: false,
+  };
+  componentDidCatch(error, info) {
+    debugger;
+    this.setState({ hasError: true });
+  }
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+    return (
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList
+            onItemSelected={this.onPersonSelected}
+            getData={this.swapiService.getAllPeople}
+          />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails personId={this.state.selectedPerson} />
+          <ErrorButton />
+        </div>
+      </div>
+    );
+  }
+}
